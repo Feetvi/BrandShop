cf47b7cc3b0d44ab9002a9473c244be8d5d423e4
Feetvi <razer-key@mail.ru>
2017-05-29 14:17:04 +0300

    brandshop

---
+++ templates/main.php
@@ -0,0 +1,11 @@
+<div id="promo">
+    <h1 id="promoText">
+        Новый PhoneBrand - это лучший смартфон
+    </h1>
+</div>
+
+<div id="mainTextWrap">
+    <div id="mainText">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quam sem, ultricies sit amet est ac, pellentesque accumsan ligula. Maecenas eros elit, pretium sit amet maximus et, bibendum placerat mauris. Aenean hendrerit elit vitae turpis accumsan, ut imperdiet turpis aliquet. Suspendisse vel molestie tortor, eu blandit nunc. Donec sit amet libero sollicitudin, dapibus eros a, consectetur tortor. Pellentesque gravida leo vitae tortor pretium imperdiet. Phasellus quis ex imperdiet tellus ullamcorper vulputate. Duis sagittis consectetur risus, et tempor ligula imperdiet nec. Sed at libero sit amet nisl laoreet vulputate. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos.
+    </div>
+</div>
